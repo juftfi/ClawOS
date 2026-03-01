293cd894940d45801d693d414a21dab1f7ba16aa
juftfi <fourswapxyz@gmail.com>
2026-03-01 09:38:50 +0700

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
                             <Link href="#contact" className="transition hover:text-white">
                                 Contact
                             </Link>
-                            <Link href="https://flap.sh/" target="_blank" className="transition hover:text-white">
+                            <Link href="https://four.meme/" target="_blank" className="transition hover:text-white">
                                 $ClawOS
                             </Link>
                         </div>
